91754a940acae395a55de12ca19fd88efcb9aa51
karleone44 <visoroman@gmail.com>
2025-05-11 17:28:04 +0300

    Finalize exchange_manager.py with multi-exchange support

--- exchange_manager.py
+++ exchange_manager.py
@@ -1,16 +1,37 @@
+# exchange_manager.py
+
+import os
 import ccxt
 
 class ExchangeManager:
-    """C12: Менеджер для багатьох бірж"""
-    def __init__(self, configs):
-        # configs: { 'binance': {...}, 'kucoin': {...}, ... }
-        self.exchanges = {
-            name: getattr(ccxt, name)(cfg)
-            for name, cfg in configs.items()
-        }
+    """C12: Менеджер multi-exchange клієнтів (Binance, KuCoin тощо)."""
 
-    def fetch_all_tickers(self):
-        data = {}
-        for name, ex in self.exchanges.items():
-            data[name] = ex.fetch_tickers()
-        return data
+    def __init__(self, config):
+        self.clients = {}
+        exch_cfg = config.get("exchanges", {})
+
+        # Binance
+        bin_cfg = exch_cfg.get("binance", {})
+        if bin_cfg.get("api_key") and bin_cfg.get("secret"):
+            self.clients["binance"] = ccxt.binance({
+                "apiKey": os.getenv("BINANCE_API_KEY"),
+                "secret": os.getenv("BINANCE_SECRET"),
+                "enableRateLimit": True,
+            })
+
+        # KuCoin
+        ku_cfg = exch_cfg.get("kucoin", {})
+        if ku_cfg.get("api_key") and ku_cfg.get("secret"):
+            self.clients["kucoin"] = ccxt.kucoin({
+                "apiKey": os.getenv("KUCOIN_API_KEY"),
+                "secret": os.getenv("KUCOIN_SECRET"),
+                "enableRateLimit": True,
+            })
+
+    def get(self, name="binance"):
+        """Повернути клієнт заданої біржі (за замовчуванням Binance)."""
+        return self.clients.get(name)
+
+    def list_exchanges(self):
+        """Повернути список доступних бірж."""
+        return list(self.clients.keys())
