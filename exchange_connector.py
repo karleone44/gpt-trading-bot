efac2f42fc85688888aa8321765395772761e92a
karleone44 <visoroman@gmail.com>
2025-05-11 12:32:57 +0300

    Update exchange_connector.py

--- exchange_connector.py
+++ exchange_connector.py
@@ -3,13 +3,13 @@
 from dotenv import load_dotenv
 import os, ccxt
 
-# Підвантажуємо змінні з .env (де лежать ваші API-ключі)
+# Підвантажуємо змінні з .env
 load_dotenv()
 
 def get_client():
     return ccxt.binance({
-        'apiKey': os.getenv('BINANCE_API_KEY'),
-        'secret': os.getenv('BINANCE_SECRET'),
+        'apiKey':    os.getenv('BINANCE_API_KEY'),
+        'secret':    os.getenv('BINANCE_SECRET'),
         'enableRateLimit': True,
     })
 
