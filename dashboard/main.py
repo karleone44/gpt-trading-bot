95f457ef86fe1faf5556235c389dd4caf240d9a6
karleone44 <visoroman@gmail.com>
2025-05-11 13:51:11 +0300

    Fix PEP8 in dashboard/main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'status':'ok'}
 
-# TODO: додати ендпоінти для latency, P&L, Sharpe, health checks
+@app.get("/")
+async def root():
+    return {"status": "ok"}
