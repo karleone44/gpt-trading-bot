7f291c4a4e108a6aba24f0085b3ce4a2a6d07a56
karleone44 <visoroman@gmail.com>
2025-05-11 13:53:00 +0300

    Fix PEP8 in exchange_connector.py

--- exchange_connector.py
+++ exchange_connector.py
@@ -1,18 +1,18 @@
-# exchange_connector.py
-
 from dotenv import load_dotenv
-import os, ccxt
+import os
+import ccxt
 
-# Підвантажуємо змінні з .env
+
 load_dotenv()
+
 
 def get_client():
     return ccxt.binance({
-        'apiKey':    os.getenv('BINANCE_API_KEY'),
-        'secret':    os.getenv('BINANCE_SECRET'),
-        'enableRateLimit': True,
+        "apiKey": os.getenv("BINANCE_API_KEY"),
+        "secret": os.getenv("BINANCE_SECRET"),
+        "enableRateLimit": True,
     })
 
-if __name__ == '__main__':
-    client = get_client()
-    print(client.fetch_balance())
+
+if __name__ == "__main__":
+    print(get_client().fetch_balance())
