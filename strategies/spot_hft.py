4ac25913e1c8a25756cfdf1e65ca1feda4d51b08
karleone44 <visoroman@gmail.com>
2025-05-11 04:15:18 +0300

    Create spot_hft.py

---
+++ strategies/spot_hft.py
@@ -0,0 +1,18 @@
+# strategies/spot_hft.py
+
+class SpotHFT:
+    def __init__(self, client, config):
+        self.client = client
+        self.config = config
+
+    def generate_signals(self, ticker_data):
+        bid = ticker_data['bid']
+        ask = ticker_data['ask']
+        spread = (ask - bid) / ask
+        threshold = self.config.get('spread_threshold', 0.001)
+        signals = []
+        if spread <= threshold:
+            signals.append({'side': 'buy', 'price': ask})
+        elif spread >= threshold:
+            signals.append({'side': 'sell', 'price': bid})
+        return signals
